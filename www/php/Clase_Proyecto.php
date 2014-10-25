804b1e86549cbde747f32f4bb441f094f8d8651e
msgonzalez <msgonzalez@esei.uvigo.es>
2014-10-25 10:18:13 +0200

    EliminarProyecto-Jesus

--- www/php/Clase_Proyecto.php
+++ www/php/Clase_Proyecto.php
@@ -89,91 +89,11 @@ function Alta_Proyecto(){
 			header('location: /Error_Alta_Proyecto.php');
 		}
 }
-
-function Baja_Proyecto()
-{
-
-    ConectarDB();
-    $sql = "select * from Proyecto where Nombre_Proyecto = '".$this->Nombre_Proyecto."'";
-    $resultado = mysql_query($sql);
-    if (mysql_num_rows($resultado) == 1)
-    {
-        $sql = "delete from Proyecto where Nombre_Proyecto = '".$this->Nombre_Proyecto."'";
-        mysql_query($sql);
-    echo "<br>o Nombre_Proyecto con Nombre = '".$this->Nombre_Proyecto."' fue borrado correctamente.<br>";
-	header('Location: /ListadoProyectos.php');
-    }
-    else
-        echo "<br>O '".$this->Nombre_Proyecto."' no existe.<br>";
-}
-
-//funcion Consultar: hace una búsqueda en la tabla Proyecto con
-//los datos del Proyecto Si van vacios devuelve todos
-function Consultar_Proyecto($Nombre_Proyecto)
-{
-    ConectarDB();
-    $sql = "select * from Proyecto where Nombre_Proyecto = '%".$Nombre_Proyecto."%'";
-    $resultado = mysql_query($sql);
-	// llamada metodo mostrar Proyecto
-	$this->Mostrar_Proyecto($resultado);
-}
-
-//Presenta en pantalla los datos que se le pasan en un recordset
-function Mostrar_Proyecto($resultado)
-{
-    echo "Nombre_Proyecto"."------"."ID_Usuario"."------"."Prioridad_Proyecto"."<br>";
-    while ($Proyecto = mysql_fetch_array($result))
-    {
-//    echo "<a href='ModBorr.php?Login_Proyecto=".$Login_Proyecto['Login_Proyecto']."'>";
-    echo $Proyecto['Nombre_Proyecto'];
-	echo "-------".$Proyecto['ID_Usuario'];
-    echo "-------".$Proyecto['Descripcion_Proyecto'];
-   
-    echo "<br>";
-    }
-}
-
-function RellenaDatos()
-{
-    ConectarDB();
-    $sql = "select * from Proyecto where Nombre_Proyecto = '".$this->Nombre_Proyecto."'";
-    $resultado = mysql_query($sql);
-    $Proyecto = mysql_fetch_array($resultado);
-	
-    $this->Nombre_Proyecto			= $Nombre_Proyecto;
-	$this->ID_Usuario				= $ID_Usuario;
-	$this->Nombre_Proyecto 			= $Prioridad_Proyecto;
-   
-}
-
-function Modificar_Proyecto()
-{
-    ConectarDB();
-    $sql = "select * from Proyecto where Nombre_Proyecto = '".$this->Nombre_Proyecto."'";
-    $resultado = mysql_query($sql);
-    if (mysql_num_rows($resultado) == 1)
-    {
-        $sql = "UPDATE Proyecto SET Prioridad_Proyecto = '".$this->Nombre_Proyecto."'";
-        echo $sql;
-        mysql_query($sql);
-		header ('Location: /ListadoProyectos.php');
-    }
-    else
-    echo "<br>o Nombre_Proyecto con valor '".$this->Nombre_Proyecto."' no existe<br>";
-	header ('Location: /ListadoProyectos.php');
-}
-
-//  Lista todos los proyectos de un usuario.
-
-
-
-
-
 }//fin de clase
 
 
 function ListarProyectos($ID_Usuario){
-ConectarDB();
+ConectarDB();	
 $sql = "select * from Proyecto where ID_Usuario = '$ID_Usuario' order by Prioridad_Proyecto";
 $resultado=mysql_query($sql) or die(mysql_error());
 while($row = mysql_fetch_array($resultado))
@@ -200,8 +120,10 @@ while($row = mysql_fetch_array($resultado))
 									echo "Sin prioridad";
 								}
 								echo "</td>";
-								echo "<td width='20%'><button type='submit' name='accion'>Editar</button></td>";
-								echo "</td>";
+										echo "<td width='20%'><button type='submit' name='accion'>Editar</button>
+												<button type='submit' name='delete'>Borrar</button>
+												</td>";
+									echo "</td>";
                        		echo "</tr>";
 							echo "</form>";
 							}
@@ -217,4 +139,30 @@ $resultado=mysql_query($sql) or die(mysql_error());
 	}
 }
 
+// Eliminar proyecto
+	if (array_key_exists('delete', $_POST)) {
+		$conn = null;
+		$host = 'localhost';
+		$db =   'ToDo_DB';
+		$user = 'Admin_ToDo';
+		$pwd =  'Pass_ToDo';
+		try {
+			$conn = new PDO('mysql:host='.$host.';dbname='.$db, $user, $pwd);
+		}
+		catch (PDOException $e) {
+			echo '<p>Error de seleccion de la BD !!</p>';
+			exit;
+		}
+		$OK = true;
+		$sql = 'DELETE FROM Proyecto WHERE Nombre_Proyecto = ?';
+		$stmt = $conn->prepare($sql);
+		$OK = $stmt->execute(array($_GET['Nombre_Proyecto']));
+		$error = $stmt->errorInfo();
+		if (!$OK) {
+			echo $error[2];
+		} else {
+			header('Location: /ListadoProyectos.php');
+			exit;
+		}
+	}
 ?>
